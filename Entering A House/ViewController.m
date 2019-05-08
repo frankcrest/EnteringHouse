19f18c82be88afdf0e5ce15291ff87ef283d4cea
Frank Chen <frankcrestview@hotmail.com>
2019-05-08 16:28:23 -0400

    Initial Commit

---
+++ Entering A House/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Entering A House
+//
+//  Created by Frank Chen on 2019-05-08.
+//  Copyright © 2019 Frank Chen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
